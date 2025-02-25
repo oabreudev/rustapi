92b6ad01b9c7cd1945f699fee0072a732f74115d
Seu Nome <deividlourencoabreu.smi@gmail.com>
2025-02-25 14:01:09 -0300

    SUPORTE CORS

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ mod models;
 mod routes;
 mod db;
 
+use rocket::http::Method;
+use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
 use rocket_sync_db_pools::database;
 
 #[database("sqlite_database")]
@@ -16,8 +18,24 @@ pub struct DbConn(diesel::SqliteConnection);
 #[launch]
 fn rocket() -> _ {
     dotenv::dotenv().ok();
+    
+    // Configuração CORS para permitir todas as origens
+    let cors = CorsOptions {
+        allowed_origins: AllowedOrigins::all(), // Permite qualquer origem
+        allowed_methods: vec![Method::Get, Method::Post, Method::Put, Method::Delete]
+            .into_iter()
+            .map(From::from)
+            .collect(),
+        allowed_headers: AllowedHeaders::all(), // Permite todos os headers
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("Erro ao configurar CORS");
+    
     let rocket = rocket::build()
         .attach(DbConn::fairing())
+        .attach(cors) // Adiciona o CORS como um Fairing
         .mount("/", routes![
             routes::get_by_cpf,
             routes::get_by_email,
